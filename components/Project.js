648acd3abab9d65d1f24aec307d243cc66080d38
brandoyts <brando.endona.dev@gmail.com>
2021-03-16 10:43:03 +0800

    update project description

--- components/Project.js
+++ components/Project.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 import { BiCodeAlt } from "react-icons/bi";
 import { CgBrowser } from "react-icons/cg";
 import styles from "../styles/Project.module.css";
